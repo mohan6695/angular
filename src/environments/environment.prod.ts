09604352d200afe144f4748fc61576e469b7015c
Mohana Krishna <mohankrishna6695@gmail.com>
2021-04-05 22:15:00 -0400

    changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  submitUrl : "http://34.69.145.117:32461/SampleRest/resources/restService/add",
+  idsUrl : "http://34.69.145.117:32461/SampleRest/resources/restService/ids",
+ production: true
+};
